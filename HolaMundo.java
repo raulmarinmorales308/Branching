37747ddef41a98fc8d8afb228e2794711a014bb2
raulmarinmorales308 <rmarmor308@iesalmudeyne.es>
2024-10-16 08:33:57 +0200

    Version inicial del fichero HolaMundo.java sin colores

---
+++ HolaMundo.java
@@ -0,0 +1,6 @@
+public class HolaMundo{
+    public static void main (String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Aqui estamos haciendo las pruebas con Git");
+    }
+}
