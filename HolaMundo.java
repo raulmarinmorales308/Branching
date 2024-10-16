0885ea27360076504bb98df7bb9829693fd49ec0
raulmarinmorales308 <rmarmor308@iesalmudeyne.es>
2024-10-16 08:48:05 +0200

    Version del fichero HolaMundo.java con colores

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,5 +2,10 @@ public class HolaMundo{
     public static void main (String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Aqui estamos haciendo las pruebas con Git");
+        String rojo = "/033[32m";
+        String azul = "/033[34m";
+        System.out.println("Hola mundo!");
+        System.out.println("Aqui estamos haciendo pruebas en Git.");
+
     }
 }
