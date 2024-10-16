98fb7733db2c1c76662ea6e3d4f57915728c8357
raulmarinmorales308 <rmarmor308@iesalmudeyne.es>
2024-10-16 09:32:10 +0200

    Se ha cambiado el codigo por el correcto

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,8 +2,8 @@ public class HolaMundo{
     public static void main (String[] args) {
         System.out.println("Hola Mundo");
         System.out.println("Aqui estamos haciendo las pruebas con Git");
-        String rojo = "/033[32m";
-        String azul = "/033[34m";
+        String rojo = "\033[31m";
+        String azul = "\033[34m";
         System.out.println(rojo + "Hola mundo!");
         System.out.println(azul + "Aqui estamos haciendo pruebas en Git.");
 
